af5233da157d641a2ecf5a29313b0508fb48470d
hirotask <hirotaskhashi@gmail.com>
2022-05-04 14:20:35 +0900

    [refactor] onProjectileHitEventを移動

--- src/main/java/jp/ne/sakura/erudoblog/ninjaoni2/ninjaoni2/listener/NinjaItemListener.java
+++ src/main/java/jp/ne/sakura/erudoblog/ninjaoni2/ninjaoni2/listener/NinjaItemListener.java
@@ -8,16 +8,15 @@ import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.inventory.NinjaInventory;
 import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.inventory.item.NinjaItem;
 import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.utils.GameState;
 import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.inventory.ItemManager;
+import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.utils.MessageManager;
 import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.utils.Ninja;
 import jp.ne.sakura.erudoblog.ninjaoni2.ninjaoni2.utils.Teams;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -34,6 +33,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+//アイテム処理に関するリスナー
 public class NinjaItemListener implements Listener {
 
     private NinjaOni2 plugin;
@@ -156,6 +156,84 @@ public class NinjaItemListener implements Listener {
             ItemStack item = e.getItemDrop().getItemStack().clone();
             e.getItemDrop().remove();
             player.getInventory().addItem(item);
+        }
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent e) {
+        if (plugin.getGameState() != GameState.INGAME) {
+            return;
+        }
+
+        //クナイの処理
+        if (e.getEntity() instanceof Arrow) {
+            if (e.getHitEntity() == null) {
+                e.getEntity().remove();
+                e.setCancelled(true);
+            } else {
+                Arrow arrow = (Arrow) e.getEntity();
+                Player player = (Player) e.getHitEntity();
+
+                if(arrow.getShooter() == null) {
+                    return;
+                }
+
+                if(!(arrow.getShooter() instanceof Player)) {
+                    return;
+                }
+
+                if(!NinjaOni2.containsNinja(player)) {
+                    return;
+                }
+
+                Player shooter = (Player) arrow.getShooter();
+                Ninja ninja =NinjaOni2.getNinjaPlayer(player);
+
+                if(ninja.getTeam() != Teams.PLAYER) {
+                    return;
+                }
+
+                if(!ninja.isLocked()) {
+                    e.setCancelled(true);
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE, 0.3F, 1);
+                    MessageManager.sendAll(ChatColor.RED + ninja.getPlayer().getName() + ChatColor.WHITE + "は" + ChatColor.DARK_AQUA + shooter.getName() + ChatColor.WHITE + "に確保された！");
+                    ninja.setLocked(true);
+                }
+            }
+        }
+
+        //粘着玉の処理
+        if(e.getEntity() instanceof Snowball) {
+            if (e.getHitEntity() == null) {
+                e.getEntity().remove();
+                e.setCancelled(true);
+            } else {
+                Snowball snowball = (Snowball) e.getEntity();
+                Player player = (Player) e.getHitEntity();
+
+                if(snowball.getShooter() == null) {
+                    return;
+                }
+
+                if(!(snowball.getShooter() instanceof Player)) {
+                    return;
+                }
+
+                if(!NinjaOni2.containsNinja(player)) {
+                    return;
+                }
+
+                Player shooter = (Player) snowball.getShooter();
+                Ninja ninja =NinjaOni2.getNinjaPlayer(player);
+
+                if(ninja.getTeam() != Teams.ONI) {
+                    return;
+                }
+
+                shooter.playSound(shooter.getLocation(),Sound.BLOCK_SLIME_BLOCK_STEP, 0.8f,1f);
+                player.playSound(player.getLocation(),Sound.BLOCK_SLIME_BLOCK_STEP, 0.8f,1f);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * 5, 4),true);
+            }
         }
     }
 
